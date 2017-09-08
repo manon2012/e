0eba4cd52698e297981badc0905b4f48d99975dc
manon2012 <manon2012@126.com>
2017-09-08 12:52:51 +0800

    good version

--- company/ctwodeep.py
+++ company/ctwodeep.py
@@ -1,20 +1,38 @@
 
 menu = {'a':{
-             'a1': 1,
-             'a2':2
+             'a1': {},
+             'a2':{}
           },
         'b':{
-              'b1':1,
-              'b2':2
+              'b1':{},
+              'b2':{}
         }
 }
 
-while True:
-    for i in menu:
-        print i
-    choice = raw_input("please input your choice: ")
+# while True:
+#     for i in menu:
+#         print i
+#     choice = raw_input("please input your choice: ")
 
-    if choice in menu:
-        for i in menu[choice]:
-            print i
-    choice = raw_input("please input your new choice: ")
+#     if choice in menu:
+#         for i in menu[choice]:
+#             print i
+#     choice = raw_input("please input your new choice, q for exit: ")
+#     if 
+
+currentlayer = menu
+qlist = []
+
+while True:
+    for key in currentlayer:
+        print key
+    choice = raw_input(">>").strip()
+
+    if choice in currentlayer:
+        qlist.append(currentlayer)
+        currentlayer=currentlayer[choice]
+    if choice == 'r':
+        if qlist:
+            currentlayer = qlist.pop()
+    if choice == 'q':
+        break
