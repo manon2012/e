598f46b23b178fa95a78130c4d39341da7bbef35
manon2012 <manon2012@126.com>
2017-09-14 18:15:17 +0800

    need more code

--- company/cmofa.py
+++ company/cmofa.py
@@ -15,3 +15,21 @@ f1 = Foo("tom",21)
 f1()
 
 
+class DictDemo:
+    def __init__(self, key,value):
+        self.dict ={}
+        self.dict[key] =  value
+
+    def __getitem__(self,key):
+        return self.dict[key]
+
+    def __setitem__(self,key,value):
+        self.dict[key] = value
+
+d1 =DictDemo("tom",21)
+
+print d1["tom"]
+
+d1["jack"] = 31
+print d1["jack"]
+
