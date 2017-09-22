0e6f576593ea9c5c0739726581a776d9e55f2a17
manon2012 <manon2012@126.com>
2017-09-22 12:59:37 +0800

    begin sort,c003, company

---
+++ company/c003sort.py
@@ -0,0 +1,10 @@
+alist = [6,2,5,3,60]
+
+for i in range(len(alist)):
+    for j in range(len(alist))[1:]:
+        if alist[i]  > alist[j]:
+            tmp= alist[i]
+            alist[i] = alist[j]
+            alist[j] =tmp
+
+print  alist
