077a0ecb6faab65fd08a036f5103535394b6d40d
manon2012 <manon2012@126.com>
2017-09-22 13:26:30 +0800

    bubble done company.

--- company/c003sort.py
+++ company/c003sort.py
@@ -1,7 +1,7 @@
-alist = [6,2,5,3,60]
+alist = [1000,6,2,5,3,60]
 
 for i in range(len(alist)):
-    for j in range(len(alist))[1:]:
+    for j in range(len(alist))[i+1:]:
         if alist[i]  > alist[j]:
             tmp= alist[i]
             alist[i] = alist[j]
